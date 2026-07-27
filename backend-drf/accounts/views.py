1a295d98d1b47053311ec5c9ff106108f5777364
fahim01330 <fahim.tecno@gmail.com>
2026-07-28 01:18:16 +0600

    User register to frontend and data backend catch.

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
-from .serializers import UserSerializers
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
+from .serializers import UserSerializers
 
-# Create your views here.
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializers
-    permission_classes = [AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        print(serializer.errors)   # Terminal-এ error দেখাবে
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- stock_prediction_portal/src/App.jsx
+++ stock_prediction_portal/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './CSS/Style.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './components/Main'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
-      <Main />
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </>
   )
 }
